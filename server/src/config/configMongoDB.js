d30a64e2b93fe13677b5967c41e715d5c704c0a5
Piero Estevez <pieroestevez88@gmail.com>
2024-06-21 17:49:44 -0300

    prueba BD

--- server/src/config/configMongoDB.js
+++ server/src/config/configMongoDB.js
@@ -9,7 +9,9 @@ const dbmongo_db = process.env.DB_DATABASE;
 
 const MONGODB_URL = `mongodb://${dbmonto_host}/${dbmongo_db}`;
 
-const MONGODB_URI = process.env.DB_MONGO_URI;
+const MONGODB_URI = "mongodb://uiejasvhmqqtl6flmnbm:oTykw5sEpkb1AxEfVBL@bjypsgdvmou5kshd3vfp-mongodb.services.clever-cloud.com:2663/bjypsgdvmou5kshd3vfp"
+
+//process.env.DB_MONGO_URI;
 
 mongoose
   .connect(MONGODB_URI, {})
