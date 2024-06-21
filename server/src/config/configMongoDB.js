94cab3d8764ec6d500a3935da989660b7f4296fb
Piero Estevez <pieroestevez88@gmail.com>
2024-06-21 17:21:23 -0300

    hosting final

--- server/src/config/configMongoDB.js
+++ server/src/config/configMongoDB.js
@@ -9,9 +9,9 @@ const dbmongo_db = process.env.DB_DATABASE;
 
 const MONGODB_URL = `mongodb://${dbmonto_host}/${dbmongo_db}`;
 
-//const MONGODB_URL = process.env.DB_MONGO_URI;
+const MONGODB_URI = process.env.DB_MONGO_URI;
 
 mongoose
-  .connect(MONGODB_URL, {})
+  .connect(MONGODB_URI, {})
   .then((bd) => console.log("Conexión exitosa a la BD de Mongoose"))
   .catch((err) => console.log("Error de Conexión db ", err));
