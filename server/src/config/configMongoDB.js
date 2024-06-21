7ee28f74a1d8e899d7b9d392b50c6d681b63097a
Piero Estevez <pieroestevez88@gmail.com>
2024-06-21 17:59:40 -0300

    ya estoy cansado jefe

--- server/src/config/configMongoDB.js
+++ server/src/config/configMongoDB.js
@@ -7,13 +7,11 @@ require("dotenv").config({
 const dbmonto_host = process.env.DB_MONGO_HOST;
 const dbmongo_db = process.env.DB_DATABASE;
 
-const MONGODB_URL = `mongodb://${dbmonto_host}/${dbmongo_db}`;
+//const MONGODB_URL = `mongodb://${dbmonto_host}/${dbmongo_db}`;
 
-const MONGODB_URI = "mongodb://uiejasvhmqqtl6flmnbm:oTykw5sEpkb1AxEfVBL@bjypsgdvmou5kshd3vfp-mongodb.services.clever-cloud.com:2663/bjypsgdvmou5kshd3vfp"
-
-//process.env.DB_MONGO_URI;
+const MONGODB_URL = process.env.DB_MONGO_URI;
 
 mongoose
-  .connect(MONGODB_URI, {})
+  .connect(MONGODB_URL, {})
   .then((bd) => console.log("Conexión exitosa a la BD de Mongoose"))
   .catch((err) => console.log("Error de Conexión db ", err));
